e4e00035f948c3f40e362f8e1a81cf022df9a7cb
7kia <il7kia@yandex.ru>
2016-06-08 14:29:08 +0400

    Add opportunity read from file

--- Lexer/main.cpp
+++ Lexer/main.cpp
@@ -7,12 +7,46 @@
 #include <iostream>
 #include <time.h>
 
-int main()
-{
-    std::srand(static_cast<unsigned>(time(nullptr)));
-    CInterpreter interpreter(std::cout, std::cerr);
+#include "main.h"
 
-    interpreter.EnterLoop(std::cin);
+using namespace std;
+
+void CheckParametrs(int argc)
+{
+	if (argc != AMOUNT_ARGUMENTS)
+	{
+		throw invalid_argument(MESSAGE_INCORRECT_AMOUNT_ARGUMENTS + to_string(AMOUNT_ARGUMENTS));
+	}
+}
+
+int main(int argc, char *argv[]) 
+{
+	try
+	{
+		CheckParametrs(argc);
+	}
+	catch (const std::invalid_argument& e)
+	{
+		std::srand(static_cast<unsigned>(time(nullptr)));
+		CInterpreter interpreter(std::cout, std::cerr);
+
+		interpreter.EnterLoop(std::cin);
+
+		return 0;
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+		return 1;
+	}
+    
+
+	ofstream outputFile(argv[3]);
+	ofstream errorFile(argv[2]);
+	ifstream inputFile(argv[1]);
+
+	CInterpreter interpreter(outputFile, errorFile);
+	interpreter.EnterLoop(inputFile);
 
     return 0;
 }
