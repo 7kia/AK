95c3a10963b1896dad26f73d2c202d71a9d7c176
7kia <il7kia@yandex.ru>
2016-11-06 20:43:34 +0300

    Add nodes for arithmetic expressions

---
+++ Lexer/src/CalcContext.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "expression.h"
+
+/** Calculator context used to save the parsed expressions. This context is
+* passed along to the example::Driver class and fill during parsing via bison
+* actions. */
+class CalcContext
+{
+public:
+	/// free the saved expression trees
+	~CalcContext()
+	{
+		clearExpressions();
+	}
+	//////////////////////////////////////////////////////////////////////
+	// Methods
+public:
+	/// free all saved expression trees
+	void	clearExpressions()
+	{
+		for (unsigned int i = 0; i < expressions.size(); ++i)
+		{
+			delete expressions[i];
+		}
+		expressions.clear();
+	}
+
+	/// check if the given variable name exists in the storage
+	bool	existsVariable(const std::string &varname) const
+	{
+		return variables.find(varname) != variables.end();
+	}
+
+	/// return the given variable from the storage. throws an exception if it
+	/// does not exist.
+	double	getVariable(const std::string &varname) const
+	{
+		variablemap_type::const_iterator vi = variables.find(varname);
+		if (vi == variables.end())
+			throw(std::runtime_error("Unknown variable."));
+		else
+			return vi->second;
+	}
+
+	//////////////////////////////////////////////////////////////////////
+	// Data
+public:
+	/// type of the variable storage
+	typedef std::map<std::string, double> variablemap_type;
+
+	/// variable storage. maps variable string to doubles
+	variablemap_type		variables;
+
+	/// array of unassigned expressions found by the parser. these are then
+	/// outputted to the user.
+	std::vector<CalcNode*>	expressions;
+
+};
---
+++ Lexer/src/Node.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <map>
+#include <vector>
+#include <ostream>
+#include <stdexcept>
+#include <cmath>
+
+/** CalcNode is the abstract base class for calculation nodes. From it the
+* different nullary, unary and binary nodes are derived. */
+class CalcNode
+{
+public:
+	/// required for virtual functions. in the derived classes the operands are
+	/// deleted.
+	virtual ~CalcNode()
+	{
+	}
+//////////////////////////////////////////////////////////////////////
+// Methods
+public:
+	/// evaluate the complete calculation tree and return the floating point
+	/// result value
+	virtual double	evaluate() const = 0;
+
+	/// output the calculation tree to the given stream. tries to format the
+	/// output to make tree levels visible.
+	virtual void	print(std::ostream &os, unsigned int depth = 0) const = 0;
+
+	/// helper function for print() which makes the indention string
+	static inline std::string indent(unsigned int d)
+	{
+		return std::string(d * 2, ' ');
+	}
+};
