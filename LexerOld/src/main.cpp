6ab90a1cc8a79d6146ca80e1eca0517cc787e9b7
7kia <il7kia@yandex.ru>
2016-12-24 19:23:15 +0300

    Add ToRecognizeNotTerminal, check it work

--- LexerOld/src/main.cpp
+++ LexerOld/src/main.cpp
@@ -25,12 +25,54 @@ bool ToRecognizeTerminal(Terminal* terminal, const SToken & token)
 {
 	if (terminal->m_tokenId == token.id)
 	{
-		//return std::make_shared<RecognizeTerminal>(token);
 		return true;
 	}
 
-	//throw CUnexpectedSymbolsError(token.id, terminal->m_tokenId);
 	return false;
+}
+
+bool ToRecognizeNotTerminal(NotTerminal* terminal, std::vector<SToken> & tokenStack)
+{
+	bool result = true;
+	bool equalThisRule = true;
+	size_t index = 0;
+	for (const auto ruleLine : terminal->m_ruleElements)
+	{
+		for (const auto element : ruleLine)
+		{
+			auto typeElement = element->m_type;
+
+			////////////////////
+			// Check element rule
+			if (typeElement == IRuleElement::Type::NotTerminal)
+			{
+				equalThisRule = ToRecognizeNotTerminal(dynamic_cast<NotTerminal*>(element.get()), tokenStack);
+				if (!equalThisRule)
+				{
+					index = 0;
+					break;
+				}
+			}
+			else
+			{
+				equalThisRule = ToRecognizeTerminal(dynamic_cast<Terminal*>(element.get()), tokenStack[index++]);
+				if (!equalThisRule)
+				{
+					index = 0;
+					break;
+				}
+			}
+		}
+		////////////////////
+
+		if (equalThisRule)
+		{
+			break;
+		}
+	}
+
+
+	return equalThisRule;
 }
 
 // TODO : transfer to other place
@@ -72,17 +114,27 @@ int main(int argc, char *argv[])
 	*/
 	PTerminal Id = std::make_shared<Terminal>(TokensId::TK_ID);
 	PTerminal Plus = std::make_shared<Terminal>(TokensId::TK_PLUS);
+	PTerminal Integer = std::make_shared<Terminal>(TokensId::TK_INTEGER);
 
-	NotTerminal Axiom("Axiom", {
-		{ Id }
-		, { Id , Plus , Id }
-	});
+
+	const std::vector<RuleElements> vector =  {
+				{ Id , Plus , Id }
+			,	{ Integer , Plus , Id }
+			,	{ Id , Plus , Integer }
+			,	{ Id }
+
+	};
+	PNotTerminal Axiom = std::make_shared<NotTerminal>(
+		"Axiom"
+		, vector
+		);
 
 	std::vector<SToken> tokens = { SToken("id", TokensId::TK_ID)
 								, SToken("+", TokensId::TK_PLUS)
 								, SToken("id", TokensId::TK_ID) };
 	
-	auto inputToken = SToken("id", TokensId::TK_ID);
+	/*
+		auto inputToken = SToken("id", TokensId::TK_ID);
 	auto pTerminal = dynamic_cast<Terminal*>(Axiom.m_ruleElements[0][0].get());
 	bool isRecognize = ToRecognizeTerminal(pTerminal
 											, inputToken);
@@ -90,6 +142,18 @@ int main(int argc, char *argv[])
 	inputToken = SToken("+", TokensId::TK_PLUS);
 	bool isRecognize2 = ToRecognizeTerminal(pTerminal
 		, inputToken);
+
+
+	*/
+
+	//auto pNotTerminal = dynamic_cast<NotTerminal*>(Axiom.m_ruleElements[0][0].get());
+	//bool isRecognize = ToRecognizeNotTerminal(Axiom.get(), tokens);
+
+	tokens = { SToken("2", TokensId::TK_INTEGER)
+		, SToken("+", TokensId::TK_PLUS)
+		, SToken("id", TokensId::TK_ID) };
+	bool isRecognize2 = ToRecognizeNotTerminal(Axiom.get(), tokens);
+
 
     return 0;
 }
